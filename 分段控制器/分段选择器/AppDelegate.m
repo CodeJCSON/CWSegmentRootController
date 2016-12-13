f2b47dc5563efe2a438dc78f28c6f9420b2367ae
CodeJCSON <yunmei@yunmeideiMac.local>
2016-12-13 16:35:54 +0800

    CWSegmentRootController
    CWSegmentRootController分段控制器

---
+++ 分段控制器/分段选择器/AppDelegate.m
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.m
+//  分段选择器
+//
+//  Created by kingcong on 16/6/2.
+//  Copyright © 2016年 王聪. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@end
+
+@implementation AppDelegate
+
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+
+    return YES;
+}
+
+
+@end
